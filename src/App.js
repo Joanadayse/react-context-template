3a4243517176d740c2d81108553acec34843c045
Joanadayse <daysejoana3@gmail.com>
2023-01-26 14:15:02 -0300

    pratica-guiada

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Router from "./routes/Router";
+import GlobalState from "./context/GlobalState";
 
 export default function App() {
   return (
-    <>
+    <GlobalState>
       <Router />
-    </>
+    </GlobalState>
   );
 }
